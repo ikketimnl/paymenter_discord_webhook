e19d0f065f51dfe536c82e4616c532fa260b72da
ikketim <info@timvds.nl>
2026-01-20 19:06:16 +0000

    Fix PSR-4: move DiscordWebhook to Services/ and update namespace/imports

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'paymenter/discord_ticket_webhook',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f19778e1c07ced084e6c86fceb9f3b30268df3de',
+        'type' => 'paymenter-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'paymenter/discord_ticket_webhook' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f19778e1c07ced084e6c86fceb9f3b30268df3de',
+            'type' => 'paymenter-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
